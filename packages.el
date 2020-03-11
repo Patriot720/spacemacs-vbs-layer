e75a9c09c68f21fefd6a5a2fd30917019a898ca5
Patriot720 <cerkin-3@yandex.ru>
2020-03-11 13:36:58 +0300

    update readme

--- packages.el
+++ packages.el
@@ -1,12 +1,10 @@
 (setq vbscript-packages
       '((vbs-mode :location (recipe
                              :fetcher github
-                             :repo "nverno/vbs-mode
-"))))
+                             :repo "nverno/vbs-mode"))))
 
 (defun vbscript/init-vbs-mode ()
   (use-package vbscript-mode
     :mode "\\.vbs\\'"
     :defer t
-    :init (progn
-            (vbscript-mode))))
+    :init (vbscript-mode)))
