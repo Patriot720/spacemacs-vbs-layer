039bcc449a4508ebbb69ae24348d03799f75f1f7
Patriot720 <cerkin-3@yandex.ru>
2020-03-11 13:02:18 +0300

    initial

---
+++ packages.el
@@ -0,0 +1,12 @@
+(setq vbscript-packages
+      '((vbs-mode :location (recipe
+                             :fetcher github
+                             :repo "nverno/vbs-mode
+"))))
+
+(defun vbscript/init-vbs-mode ()
+  (use-package vbscript-mode
+    :mode "\\.vbs\\'"
+    :defer t
+    :init (progn
+            (vbscript-mode))))
